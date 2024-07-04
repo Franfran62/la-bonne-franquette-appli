4a018b33bb42ff8e77fb7401bb23b3b7d0e5d3b4
Thomas Dignoire <tdignoire@protonmail.com>
2024-07-04 17:12:40 +0200

    💄AJout d'un drawer et de header aux différentes vues

---
+++ lib/widgets/side_menu_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SideMenuWidget extends StatelessWidget{
+@override
+  Widget build(BuildContext context) {
+    return const Drawer(
+      child: Column(
+        children: [
+          
+        ],
+      )
+    );
+  }
+}
