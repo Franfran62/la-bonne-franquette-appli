7d1600c70d7582e4e093b35337343503b36724f1
François K <francoiska_fr@icloud.com>
2024-07-04 15:49:25 +0200

    ApiService en static, CacheVersion devient DatabaseVersion

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -28,7 +28,7 @@ class ApiService{
     });
   }
 
-  Future<Map<String, String>> setHeaders(bool token) async {
+  static Future<Map<String, String>> setHeaders(bool token) async {
     if(token){
       String authToken = await getToken();
       return {
@@ -42,7 +42,7 @@ class ApiService{
     };
   }
 
-  Future<List<JsonCodec>> get({required String endpoint, bool token = false}) async{
+  static Future<List<JsonCodec>> get({required String endpoint, bool token = false}) async{
 
     Map<String, String> headers = await setHeaders(token);
     final response = await http.get(Uri.parse(apiQueryString + endpoint), headers: headers);
@@ -53,7 +53,7 @@ class ApiService{
     }
   }
 
-  Future<List<dynamic>> fetchAll({required String endpoint, bool token = false}) async{
+  static Future<List<dynamic>> fetchAll({required String endpoint, bool token = false}) async{
 
     Map<String, String> headers = await setHeaders(token);
     final response = await http.get(Uri.parse(apiQueryString + endpoint), headers: headers);
@@ -64,7 +64,7 @@ class ApiService{
     }
   }
   
-  Future<List<dynamic>> post({required String endpoint, required Map<dynamic, dynamic> body, bool token = false}) async{
+  static Future<List<dynamic>> post({required String endpoint, required Map<dynamic, dynamic> body, bool token = false}) async{
 
     Map<String, String> headers = await setHeaders(token);
       final response = await http.post(Uri.parse(apiQueryString + endpoint), headers: headers, body: jsonEncode(body));
@@ -75,7 +75,7 @@ class ApiService{
       }
     }
 
-  Future<List<dynamic>> put({required String endpoint, required Map<String, dynamic> body, bool token = false}) async{
+  static Future<List<dynamic>> put({required String endpoint, required Map<String, dynamic> body, bool token = false}) async{
 
     Map<String, String> headers = await setHeaders(token);
       final response = await http.put(Uri.parse(apiQueryString + endpoint), headers: headers, body: jsonEncode(body));
@@ -86,7 +86,7 @@ class ApiService{
       }      
     }
 
-  Future<bool> delete({required String endpoint, bool token = false}) async{
+  static Future<bool> delete({required String endpoint, bool token = false}) async{
     Map<String, String> headers = await setHeaders(token);
     final response = await http.delete(Uri.parse(apiQueryString + endpoint), headers: headers);
     if(response.statusCode >= 300){
@@ -96,19 +96,19 @@ class ApiService{
       }   
     }
 
-  Future<bool> connect({required User user}) async {
+  static Future<bool> connect({required User user}) async {
     Map<String, String> headers = await setHeaders(false);
     final response = await http.post(Uri.parse('$apiQueryString/auth/login'), headers: headers, body: jsonEncode(user.toJson()));
     if(response.statusCode >= 300){
       throw Exception('Erreur : Impossible de se connecter, ${response.statusCode} : ${response.body}');
     } else {
       Map<String, dynamic> token = jsonDecode(response.body);
-    SecuredStorage().writeSecrets("auth-token", token['token']);
+      SecuredStorage().writeSecrets("auth-token", token['token']);
     return true;
     }
   }
 
-  Future<String> getCacheVersion() async {
+  static Future<String> getCacheVersion() async {
     String token = await getToken();
     final response = await http.get(Uri.parse('$apiQueryString/cache/version'), headers: {
       'auth-token': token
@@ -120,8 +120,20 @@ class ApiService{
     }    
   }
 
-  static Future<bool> testConnection(String serverAddress) async {
-    final response = await http.get(Uri.parse("http://$serverAddress/api/v1/testConnection"));
-    return response.statusCode == 200;
-  }
+  static Future<bool> testConnection({String adresse = ""}) async {
+    var response;
+    if (adresse == "") {
+      String serverAddress = await SecuredStorage().readSecret("adresseServeur") as String;
+      response = await http.get(Uri.parse("http://$serverAddress/api/v1/testConnection"));
+    } else {
+      response = await http.get(Uri.parse("http://$adresse/api/v1/testConnection"));
+    }
+
+    if (response.statusCode == 200) {
+      await ApiService.setBaseAddressServer();
+      return true;
+    } else {
+      throw Exception("Impossible de se connecter au serveur car l'adresse n'est pas configurer ou le serveur est injoignable");
+    }
+  } 
 }
