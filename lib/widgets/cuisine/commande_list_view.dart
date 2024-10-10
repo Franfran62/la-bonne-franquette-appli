9e8ec800d7b87e1527227883e9d46ba5d5bdec3b
Thomas Dignoire <tdignoire@protonmail.com>
2024-10-10 18:31:04 +0200

    ♻ Création de commande_list_view.dart afin de simplifier la vue cuisine

---
+++ lib/widgets/cuisine/commande_list_view.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/commande.dart';
+import '../../services/websocket_service.dart';
+import '../../viewsmodels/cuisine/cuisinehomepage_view_model.dart';
+import 'commande_card_widget.dart';
+
+class CommandeListView extends StatefulWidget {
+
+  final CuisineHomepageViewModel viewModel = CuisineHomepageViewModel();
+
+  CommandeListView({super.key});
+
+  @override
+  _CommandeListViewState createState() => _CommandeListViewState();
+}
+
+class _CommandeListViewState extends State<CommandeListView> {
+
+  List<Commande> commandes = [];
+  final WebSocketService webSocketService = WebSocketService();
+
+  @override
+  void initState() {
+    super.initState();
+    setupWebSocket();
+    loadCommandes();
+  }
+
+  void loadCommandes() {
+    try {
+      widget.viewModel.getCommandeEnCours().then((result) {
+        setState(() {
+          commandes = result;
+        });
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  void popCommande(num id) async {
+    print(commandes.length);
+    setState(() {
+      commandes.remove(
+          commandes.firstWhere((item) => item.commandeId == id)
+      );
+      loadCommandes();
+      print(commandes.length);
+    });
+  }
+
+  void setupWebSocket() async {
+    await webSocketService.setBaseAddressServer();
+    webSocketService.connect((String message) {
+      //faire des trucs ici
+      loadCommandes();
+    });
+  }
+
+  @override
+  void dispose() {
+    webSocketService.disconnect();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: commandes.length,
+                itemBuilder: (context, index) {
+                  return ConstrainedBox(
+                      constraints: const BoxConstraints(maxWidth: 500),
+                      child: CommandeCard(commande: commandes[index], loadCommandes: loadCommandes, popCommande: popCommande,)
+                  );
+                }
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
