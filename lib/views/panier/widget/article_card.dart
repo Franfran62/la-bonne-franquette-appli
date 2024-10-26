853aaa656053b556a42d680bb5aa5b7472614af4
Thomas Dignoire <tdignoire@protonmail.com>
2024-10-26 18:19:53 +0200

    ♻️ Simplification de article_card.dart

--- lib/views/panier/widget/article_card.dart
+++ lib/views/panier/widget/article_card.dart
@@ -22,49 +22,54 @@ class ArticleCard extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: (article.ingredients.isNotEmpty || article.extraSet.isNotEmpty) ? Colors.redAccent :Colors.greenAccent,
-      child: Column(
-        children: [
-          Row(
+  return Card(
+    color: (article.ingredients.isNotEmpty || article.extraSet.isNotEmpty) ? Colors.redAccent : Colors.greenAccent,
+    child: Column(
+      children: [
+        Row(
+          children: [
+            IconButton(onPressed: ajout, icon: Icon(Icons.add)),
+            IconButton(onPressed: suppression, icon: Icon(Icons.remove)),
+            Text(article.quantite.toString()),
+            Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
+            Text(maxLines: 2, overflow: TextOverflow.visible, article.nom),
+          ],
+        ),
+        if (article.ingredients.isNotEmpty || article.extraSet.isNotEmpty)
+          Column(
             children: [
-              IconButton(onPressed: () => ajout(), icon: Icon(Icons.add)),
-              IconButton(onPressed: () => suppression(), icon: Icon(Icons.remove)),
-              Text(article.quantite.toString()),
-              Padding(padding: EdgeInsets.symmetric(horizontal: 4.0)),
-              Text(maxLines: 2, overflow: TextOverflow.visible, article.nom),
+              if (article.extraSet != null && article.extraSet.isNotEmpty)
+                ...article.extraSet.map((e) => Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 100.0),
+                          child: Icon(Icons.add),
+                        ),
+                        Text(
+                          style: TextStyle(fontSize: 14),
+                          e.nom,
+                        ),
+                      ],
+                    )),
+              if (article.ingredients != null && article.ingredients.isNotEmpty)
+                ...article.ingredients.map((e) => Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 100.0),
+                          child: Icon(Icons.remove),
+                        ),
+                        Text(
+                          style: TextStyle(fontSize: 14),
+                          e.nom,
+                        ),
+                      ],
+                    )),
             ],
           ),
-          (article!.ingredients.isNotEmpty || article!.extraSet.isNotEmpty) ? Column(
-            children: [
-              if (article?.extraSet != null && article!.extraSet.isNotEmpty) ...article.extraSet.map((e) => Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 100.0),
-                    child: Icon(Icons.add),
-                  ),
-                  Text(
-                      style: TextStyle(fontSize: 14),
-                      e.nom),
-                ],
-              )),
-              if (article?.extraSet != null && article!.ingredients.isNotEmpty) ...article.ingredients.map((e) => Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left:100.0),
-                    child: Icon(Icons.remove),
-                  ),
-                  Text(
-                    style: TextStyle(fontSize: 14),
-                      e.nom),
-                ],
-              ))
-            ],
-          ) : SizedBox(),
-        ],
-      ),
-    );
-  }
+      ],
+    ),
+  );
+}
 }
